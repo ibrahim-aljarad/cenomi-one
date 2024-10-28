c83260b27a52e009edd5b9ed8e9570e1dd2db205
arjun2 <arjun2@qburst.com>
2024-10-28 19:21:43 +0530

    api changes for discrepancy list

--- src/containers/DiscrepancyDetails/redux/reducer.ts
+++ src/containers/DiscrepancyDetails/redux/reducer.ts
@@ -1,207 +1,33 @@
 import produce from 'immer';
 
 import {
-  doApprovalAction,
-  DoApprovalActionDone,
-  doLeasingTakeAction,
-  doProucurementAction,
-  doWorkflowTakeAction,
-  getApprovalPendingTasks,
-  getApprovalTasksCount,
-  getApprovalTasksDetails,
-  getLeasingPendingTasks,
-  getLeasingTasksDetails,
-  getProcurementPendingTask,
-  getProcurementTaskDetails,
-  getUserSearchList,
-  getWorkflowPendingTasks,
-  getWorkflowTasksDetails,
-  getWorkflowUserList
+  getDiscrepancyDetail
 } from './actions';
 
-export const initialState = {
-  approvalPendingTasksData: undefined,
-  approvalTasksDetailsData: [],
-  approvalTasksAttachmentData: [],
+export type DiscrepancyDetailsData =  {
+  discrepancyDetailData: any
+}
 
-  approvalActionData: [],
-  userSearchListData: [],
-  workflowUserListData: [],
-  approvalTasksCountData: [],
-  approvalTasksCountDataLoading: false
+export const initialState: DiscrepancyDetailsData = {
+  discrepancyDetailData: {},
 };
 
 export default (state = initialState, action: { type: any; payload: { type: any } }) =>
   produce(state, (draft) => {
     switch (action.type) {
-      case getApprovalPendingTasks.TRIGGER: {
-        draft.approvalPendingTasksData = undefined;
+      case getDiscrepancyDetail.TRIGGER: {
+        draft.discrepancyDetailData = undefined;
         break;
       }
-      case getApprovalPendingTasks.SUCCESS: {
-        draft.approvalPendingTasksData = action.payload?.data;
+      case getDiscrepancyDetail.SUCCESS: {
+        draft.discrepancyDetailData = action.payload?.data;
         break;
       }
-      case getApprovalPendingTasks.FAILURE: {
-        draft.approvalPendingTasksData = [];
+      case getDiscrepancyDetail.FAILURE: {
+        draft.discrepancyDetailData = [];
         break;
       }
-
-      case getApprovalTasksDetails.TRIGGER: {
-        draft.approvalTasksDetailsData = [];
-        break;
-      }
-      case getApprovalTasksDetails.SUCCESS: {
-        draft.approvalTasksDetailsData = action.payload?.data;
-        break;
-      }
-
-      case getUserSearchList.TRIGGER: {
-        draft.userSearchListData = [];
-        break;
-      }
-      case getUserSearchList.SUCCESS: {
-        draft.userSearchListData = action.payload;
-        break;
-      }
-      case getApprovalTasksCount.TRIGGER: {
-        draft.approvalTasksCountData = [];
-        draft.approvalTasksCountDataLoading = true;
-        break;
-      }
-      case getApprovalTasksCount.SUCCESS: {
-        draft.approvalTasksCountData = action.payload;
-        break;
-      }
-      case getApprovalTasksCount.FULFILL: {
-        draft.approvalTasksCountDataLoading = false;
-        break;
-      }
-
-      case doApprovalAction.TRIGGER: {
-        draft.approvalActionData = [];
-        break;
-      }
-      case doApprovalAction.SUCCESS: {
-        draft.approvalActionData = action.payload;
-        break;
-      }
-      case DoApprovalActionDone.TRIGGER: {
-        draft.approvalTasksAttachmentData = [];
-        draft.approvalActionData = [];
-        break;
-      }
-
-      case getLeasingPendingTasks.SUCCESS: {
-        draft.approvalPendingTasksData = action.payload?.data;
-        break;
-      }
-      case getLeasingPendingTasks.TRIGGER: {
-        draft.approvalPendingTasksData = undefined;
-        break;
-      }
-      case getLeasingPendingTasks.FAILURE: {
-        draft.approvalPendingTasksData = [];
-        break;
-      }
-
-      case getLeasingTasksDetails.SUCCESS: {
-        draft.approvalTasksDetailsData = action.payload?.data;
-        break;
-      }
-      case getLeasingTasksDetails.TRIGGER: {
-        draft.approvalTasksDetailsData = [];
-
-        break;
-      }
-      case doLeasingTakeAction.TRIGGER: {
-        draft.approvalActionData = [];
-        break;
-      }
-      case doLeasingTakeAction.SUCCESS: {
-        draft.approvalActionData = action.payload;
-        break;
-      }
-
-      case getProcurementPendingTask.SUCCESS: {
-        draft.approvalPendingTasksData = action?.payload?.data || [];
-        break;
-      }
-      case getProcurementPendingTask.TRIGGER: {
-        draft.approvalPendingTasksData = undefined;
-        break;
-      }
-      case getProcurementPendingTask.FAILURE: {
-        draft.approvalPendingTasksData = [];
-        break;
-      }
-
-      case getProcurementTaskDetails.TRIGGER: {
-        draft.approvalTasksDetailsData = [];
-        break;
-      }
-      case getProcurementTaskDetails.SUCCESS: {
-        draft.approvalTasksDetailsData = action?.payload?.data || [];
-        break;
-      }
-
-      case doProucurementAction.TRIGGER: {
-        draft.approvalActionData = [];
-        break;
-      }
-      case doProucurementAction.SUCCESS: {
-        draft.approvalActionData = action.payload;
-        break;
-      }
-
-      case getWorkflowPendingTasks.TRIGGER: {
-        draft.approvalPendingTasksData = undefined;
-        break;
-      }
-      case getWorkflowPendingTasks.SUCCESS: {
-        draft.approvalPendingTasksData = action.payload?.data?.result;
-        break;
-      }
-      case getWorkflowPendingTasks.FAILURE: {
-        draft.approvalPendingTasksData = [];
-        break;
-      }
-
-      case getWorkflowTasksDetails.TRIGGER: {
-        draft.approvalTasksDetailsData = undefined;
-        break;
-      }
-      case getWorkflowTasksDetails.SUCCESS: {
-        draft.approvalTasksDetailsData = action.payload?.data?.result;
-        break;
-      }
-      case getWorkflowTasksDetails.FAILURE: {
-        draft.approvalTasksDetailsData = [];
-        break;
-      }
-
-      case getWorkflowUserList.TRIGGER: {
-        draft.workflowUserListData = undefined;
-        break;
-      }
-      case getWorkflowUserList.SUCCESS: {
-        draft.workflowUserListData = action.payload?.data?.result;
-        break;
-      }
-      case getWorkflowUserList.FAILURE: {
-        draft.workflowUserListData = [];
-        break;
-      }
-      case doWorkflowTakeAction.TRIGGER: {
-        draft.approvalActionData = [];
-        break;
-      }
-      case doWorkflowTakeAction.SUCCESS: {
-        draft.approvalActionData = action.payload;
-        break;
-      }
-
-      
+ 
       default: {
         break;
       }
