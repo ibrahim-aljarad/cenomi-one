b7c03c197d82214930b5d9718c8de338bf9afb62
SubramanyaRao11 <subramanya11rao@gmail.com>
2024-12-23 18:59:24 +0530

    Updated: New implementation to use webview for view documents

--- src/containers/Documents/DocumentView.tsx
+++ src/containers/Documents/DocumentView.tsx
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { Platform, SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
-
-import { useNavigation } from '@react-navigation/core';
-import { isEmpty } from 'lodash';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Pdf from 'react-native-pdf';
-import { Shadow } from 'react-native-shadow-2';
-import { useDispatch, useSelector } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { CustomImage, CustomText, HeaderSVG, Loader } from '../../components';
-import AppPrimaryButton from '../../components/AppPrimaryButton';
-import CustomModal from '../../components/CustomModal';
-import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../../constant';
-import { Colors, CommonStyles, Images } from '../../theme';
-import { CONFIG_CONSTANT } from '../../utils/constants';
-import { RfH, RfW, getImageUrl } from '../../utils/helper';
-import { getDocumentsDetail, submitAcknowledge } from '../Home/redux/actions';
-import { getAcknowledgeDataSelector, getDocumentsDetailsSelector } from '../Home/redux/selectors';
-import { isDarkModeSelector } from '../redux/selectors';
-import { getIsApiExecutingSelector } from '../../appContainer/redux/selectors';
-import { localize } from '../../locale/utils';
-import WrapperContainer from '../../components/WrapperContainer';
+import React, { useEffect, useState } from "react";
+import {
+  Linking,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { isEmpty } from "lodash";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import WebView from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { CustomImage, CustomText, HeaderSVG, Loader } from "../../components";
+import AppPrimaryButton from "../../components/AppPrimaryButton";
+import CustomModal from "../../components/CustomModal";
+import { SCREEN_HEIGHT } from "../../constant";
+import { Colors, CommonStyles, Images } from "../../theme";
+import { CONFIG_CONSTANT } from "../../utils/constants";
+import { RfH, RfW, getImageUrl } from "../../utils/helper";
+import { getDocumentsDetail, submitAcknowledge } from "../Home/redux/actions";
+import {
+  getAcknowledgeDataSelector,
+  getDocumentsDetailsSelector,
+} from "../Home/redux/selectors";
+import { isDarkModeSelector } from "../redux/selectors";
+import { getIsApiExecutingSelector } from "../../appContainer/redux/selectors";
+import { localize } from "../../locale/utils";
+import WrapperContainer from "../../components/WrapperContainer";
 
 const stateSelector = createStructuredSelector({
   isDarkMode: isDarkModeSelector,
   acknowledgeData: getAcknowledgeDataSelector,
   documentDetailsData: getDocumentsDetailsSelector,
-  isAPIExecuting: getIsApiExecutingSelector
+  isAPIExecuting: getIsApiExecutingSelector,
 });
 
 const DocumentView = (props: any) => {
   const { id } = props?.route?.params || {};
-
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
@@ -40,8 +46,8 @@ const DocumentView = (props: any) => {
 
   const [isCheckedAcknowledge, setIsCheckedAcknowledge] = useState(false);
   const [isMoalVisible, setIsModalVisible] = useState(false);
-  const [isSuccessSubmitAcknowledge, setIsSuccessSubmitAcknowledge] = useState(false);
-  const [signatureText, setSignatureText] = useState('');
+  const [isSuccessSubmitAcknowledge, setIsSuccessSubmitAcknowledge] =
+    useState(false);
   const [isFileLoader, setIsFileLoader] = useState(false);
   const [isApiTriggered, setIsApiTriggered] = useState(false);
 
@@ -64,11 +70,9 @@ const DocumentView = (props: any) => {
       const data = {
         featureId: documentDetailsData?.id,
         featureModule: CONFIG_CONSTANT?.DOCUMENTS,
-        metadata: {}
+        metadata: {},
       };
       dispatch(submitAcknowledge.trigger({ data }));
-
-      // setIsSuccessSubmitAcknowledge(true);
     } else {
       setIsModalVisible(true);
     }
@@ -76,60 +80,76 @@ const DocumentView = (props: any) => {
 
   const onRequestCloseModal = () => setIsModalVisible(!isMoalVisible);
 
-  const rightIconItem = (
-    <CustomImage
-      image={Images.crossBlack}
-      imageWidth={RfH(16)}
-      imageHeight={RfH(16)}
-      tintColor={isDarkMode ? Colors.white : Colors.app_black}
-    />
-  );
+  const onNavigationStateChange = (navState: any) => {
+    if (navState.url !== getImageUrl(documentDetailsData?.document?.url)) {
+      Linking.openURL(navState.url);
+      return false;
+    }
+    return true;
+  };
+
+  const injectedJavaScript = `
+    if (window.PDFViewerApplication) {
+      window.PDFViewerApplication.externalLinks.open = function(url) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'link', url: url }));
+      };
+    }
+    true;
+  `;
+
+  const onMessage = (event: any) => {
+    try {
+      const data = JSON.parse(event.nativeEvent.data);
+      if (data.type === "link") {
+        Linking.openURL(data.url);
+      }
+    } catch (error) {
+      console.error("Error handling WebView message:", error);
+    }
+  };
 
   const mainSection = () => {
     return (
       <>
         <View style={styles.documentContainer}>
-          <Pdf
-            trustAllCerts={false}
+          <WebView
             source={{
               uri: getImageUrl(documentDetailsData?.document?.url),
               headers: {},
-              cache: true
-              // headers: documentInfo?.headers
             }}
-            onLoadComplete={(numberOfPages, path) => {
-              setIsFileLoader(false);
-            }}
-            onLoadProgress={() => setIsFileLoader(true)}
-            onError={(error) => {
-              // console.log('error==========>', error);
-              setIsFileLoader(false);
-            }}
-            // style={[styles.pdf, { paddingBottom: RfH(Platform.OS === 'ios' ? 50 : 80) }]}
-            style={{
-              flex: 1,
-              width: '100%',
-              height: '100%',
-              backgroundColor: Colors.transparent,
-              paddingTop: RfH(10)
-            }}
+            style={styles.webView}
+            onLoadStart={() => setIsFileLoader(true)}
+            onLoadEnd={() => setIsFileLoader(false)}
+            onError={() => setIsFileLoader(false)}
+            onNavigationStateChange={onNavigationStateChange}
+            injectedJavaScript={injectedJavaScript}
+            onMessage={onMessage}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+            scalesPageToFit={true}
+            bounces={false}
           />
           <Loader isLoading={isAPIExecuting || isFileLoader} />
         </View>
         {documentDetailsData?.isAcknowledged === false ? (
           <View
             style={{
-              justifyContent: 'flex-end',
+              justifyContent: "flex-end",
               paddingHorizontal: RfW(24),
               paddingTop: RfH(24),
-              paddingBottom: RfH(Platform.OS === 'ios' ? 5 : 34),
-              backgroundColor: isDarkMode ? Colors.darkModeBackground : Colors.transparent,
+              paddingBottom: RfH(Platform.OS === "ios" ? 5 : 34),
+              backgroundColor: isDarkMode
+                ? Colors.darkModeBackground
+                : Colors.transparent,
               borderTopWidth: RfH(1),
-              borderTopColor: Colors.white
-            }}>
+              borderTopColor: Colors.white,
+            }}
+          >
             <TouchableOpacity
               onPress={() => setIsCheckedAcknowledge(!isCheckedAcknowledge)}
-              style={{ flexDirection: 'row', marginBottom: RfH(10) }}>
+              style={{ flexDirection: "row", marginBottom: RfH(10) }}
+            >
               <CustomImage
                 image={
                   isCheckedAcknowledge
@@ -150,13 +170,16 @@ const DocumentView = (props: any) => {
                   lineHeight: RfH(20),
                   paddingLeft: RfW(10),
                   paddingRight: RfW(10),
-                  flex: 1
-                }}>{`${localize('documents.acknowlegeAcceptationMsg')}`}</CustomText>
+                  flex: 1,
+                }}
+              >{`${localize(
+                "documents.acknowlegeAcceptationMsg"
+              )}`}</CustomText>
             </TouchableOpacity>
 
             <AppPrimaryButton
               onPress={onPressSubmit}
-              buttonText={`${localize('common.submitC')}`}
+              buttonText={`${localize("common.submitC")}`}
             />
           </View>
         ) : null}
@@ -169,16 +192,22 @@ const DocumentView = (props: any) => {
       <SafeAreaView
         style={{
           ...styles.mainContainer,
-          backgroundColor: isDarkMode ? Colors.darkModeBackground : Colors.transparent
-        }}>
+          backgroundColor: isDarkMode
+            ? Colors.darkModeBackground
+            : Colors.transparent,
+        }}
+      >
         <View
           style={{
             flex: 1,
-            backgroundColor: isDarkMode ? Colors.darkModeBackground : Colors.transparent
-          }}>
+            backgroundColor: isDarkMode
+              ? Colors.darkModeBackground
+              : Colors.transparent,
+          }}
+        >
           <HeaderSVG
             isBackButtonVisible={true}
-            titleText={documentDetailsData?.name || ''}
+            titleText={documentDetailsData?.name || ""}
             titleFont={20}
             onBackPressHandler={() => {
               navigation.goBack();
@@ -187,7 +216,10 @@ const DocumentView = (props: any) => {
           {mainSection()}
         </View>
 
-        <CustomModal modalVisible={isMoalVisible} onRequestClose={onRequestCloseModal}>
+        <CustomModal
+          modalVisible={isMoalVisible}
+          onRequestClose={onRequestCloseModal}
+        >
           <>
             <CustomText
               fontSize={14}
@@ -197,14 +229,15 @@ const DocumentView = (props: any) => {
                 lineHeight: RfH(22),
                 paddingHorizontal: RfW(30),
                 paddingTop: RfH(28),
-                textAlign: 'center'
-              }}>
-              {`${localize('documents.plzCheckTheAcknowledgeBox')}`}
+                textAlign: "center",
+              }}
+            >
+              {`${localize("documents.plzCheckTheAcknowledgeBox")}`}
             </CustomText>
 
-            <View style={{ marginTop: RfH(15), width: '100%' }}>
+            <View style={{ marginTop: RfH(15), width: "100%" }}>
               <AppPrimaryButton
-                buttonText={`${localize('common.okay')}`}
+                buttonText={`${localize("common.okay")}`}
                 onPress={onRequestCloseModal}
               />
             </View>
@@ -213,7 +246,7 @@ const DocumentView = (props: any) => {
 
         {isSuccessSubmitAcknowledge ? (
           <CustomModal
-            title={`${localize('common.submittedSuccessfully')}`}
+            title={`${localize("common.submittedSuccessfully")}`}
             modalVisible={isSuccessSubmitAcknowledge}
             onRequestClose={() => setIsSuccessSubmitAcknowledge(false)}
             onRequestActionButton={() => {
@@ -229,19 +262,20 @@ const DocumentView = (props: any) => {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
   },
   documentContainer: {
     flex: 1,
     backgroundColor: Colors.transparent,
     minHeight: SCREEN_HEIGHT - RfH(400),
-    borderRadius: RfW(6)
+    borderRadius: RfW(6),
   },
-  textInputSty: {
-    fontSize: 14,
-    color: Colors.app_black,
-    ...CommonStyles.regularFont400Style
-  }
+  webView: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: Colors.transparent,
+  },
 });
 
 export default DocumentView;
